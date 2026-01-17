878e71b21e057a92eafa6258b409379e0e026d36
kearmododragon <ckearney1992@gmail.com>
2026-01-17 12:29:47 +0100

    question logic applied, game logic stored

--- Script/script.js
+++ Script/script.js
@@ -1,11 +1,34 @@
 /* Game logic
-- Show a question
+- start screen
+- start game clicked
+- generate question screen
+- render question and answers
 - Wait for user input (button) 
-- Confirm answer
-- Update game state
-- next question or end game 
+- check if answer is correct
+- Update game state (score, highlight correct answer, any extra info)
+- repeat until 10 questions complete
+- show end game screen
 */
-
+const questions = [
+    {
+        text: "What is there more of?",
+        answers: [
+            {   text: "ants", isCorrect: true},
+            {   text: "dogs", isCorrect: false},
+            {   text: "sheep", isCorrect: false},
+            {   text: "white rhinos", isCorrect: false},
+        ]
+    },
+    {
+        text: "What colours are most common in flags of the world?",
+        answers: [
+            {   text: "Red, White, Blue", isCorrect: true},
+            {   text: "Yellow, Black", isCorrect: false},
+            {   text: "Green, Red, Black", isCorrect: false},
+            {   text: "Pink, Purple, Mauve", isCorrect: false},
+        ]
+    }
+];
 /* Rules
 - 10 questions per round
 - 4 answers
@@ -16,6 +39,12 @@
 
 */
 
+/* questions
+
+List of question objects, with text question, array of answers, known correct answer
+User clicks answer, anwer checked if correct, return correct or not
+
+*/
 
 // Functions // 
 
